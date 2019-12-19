f929ddb5854f47c1980903501924997a08422d35
ting <ting.zhang@buzzfeed.com>
2019-12-19 14:08:11 -0500

    add helper functions

--- temp.py
+++ temp.py
@@ -1,20 +1,34 @@
 import ety
+from PyDictionary import PyDictionary
 
-origin_map = {}
+
+d = PyDictionary()
+origin_descendants = {}
+
 
 for word in ety.data.etyms['eng'].keys():
     origins = ety.origins(word, recursive=True)
     for origin in origins:
         # tuple of the form ('gata', 'Old Norse')
         origin = (origin._word, origin._language.name)
-        if origin_map.get(origin):
-            origin_map[origin].append(word)
+        if origin_descendants.get(origin):
+            origin_descendants[origin].append(word)
         else:
-            origin_map[origin] = [word]
+            origin_descendants[origin] = [word]
 
-for origin in origin_map.keys():
-    if len(origin_map[origin]) > 5 and len(origin_map[origin]) < 15:
-        print(f'descendants of {origin}:')
-        for descendant in origin_map[origin]:
-            print(f'- {descendant}')
-        print('-------------')
+
+def get_origins(word):
+    origins = ety.origins(word, recursive=True)
+    return [(origin._word, origin._language.name) for origin in origins]
+
+
+def get_descendants(origin):
+    return origin_descendants.get(origin)
+
+
+def get_definition(word):
+    try:
+        definition = d.meaning(word)
+    except IndexError:
+        definition = {}
+    return definition
